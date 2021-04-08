6c1a1c6011007fd611f2707de5a2b8087e2e2c30
AndrewSimons3 <simdrew07@gmail.com>
2021-04-07 21:50:38 -0500

    understanding how elements are rendered

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
       </div>
     );
   }
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
 }
 
 export default App;
