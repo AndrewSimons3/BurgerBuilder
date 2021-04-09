48095fca8d39f95119ab38f8b404dfc5e309abc5
AndrewSimons3 <simdrew07@gmail.com>
2021-04-08 22:19:14 -0500

    updating state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,10 +5,22 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Andrew', age: 31 }
-      { name: 'Manu', age: 29 }
+      { name: 'Andrew', age: 31 },
+      { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ] 
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DONT DO THIS ----> this.state.persons[0].name = "maximilian"
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 31 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
   }
 
   render() {
@@ -16,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
