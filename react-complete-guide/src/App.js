24abece342051ce7b00cd3b2c2b552982f52f43c
AndrewSimons3 <simdrew07@gmail.com>
2021-04-08 21:32:29 -0500

    using props on component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Andrew" age="31"/>
+        <Person name="Manu" age="29" >My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
       
     );
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const person = () => {
+const person = (props) => {
   return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
 }
 
